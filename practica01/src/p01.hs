2def5f2be5a0e0927fd80308ab28a290a854464e
Daniel <daniel_lopez@ciencias.unam.mx>
2018-09-12 16:28:14 -0500

    Función mayorQue

--- practica01/src/p01.hs
+++ practica01/src/p01.hs
@@ -1,1 +1,7 @@
 data Natural = Cero | Suc Natural deriving (Eq, Show)
+
+mayorQue::Natural->Natural->Bool
+mayorQue Cero Cero = False
+mayorQue Cero _ = False
+mayorQue _ Cero = True
+mayorQue (Suc n) (Suc m) = menorque n m
