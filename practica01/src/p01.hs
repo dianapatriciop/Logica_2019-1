a8099b637472edac1399b2b8b4062fa1dada440a
Daniel <daniel_lopez@ciencias.unam.mx>
2018-09-12 16:30:09 -0500

    Función mayorQue

--- practica01/src/p01.hs
+++ practica01/src/p01.hs
@@ -4,4 +4,4 @@ mayorQue::Natural->Natural->Bool
 mayorQue Cero Cero = False
 mayorQue Cero _ = False
 mayorQue _ Cero = True
-mayorQue (Suc n) (Suc m) = menorque n m
+mayorQue (Suc n) (Suc m) = mayorQue n m
