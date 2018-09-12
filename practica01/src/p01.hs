9c2c10973eaf6b2d83db9fafbccc6d56881211fd
Daniel <daniel_lopez@ciencias.unam.mx>
2018-09-12 16:35:59 -0500

    Funciones igual y menorQue

--- practica01/src/p01.hs
+++ practica01/src/p01.hs
@@ -5,3 +5,15 @@ mayorQue Cero Cero = False
 mayorQue Cero _ = False
 mayorQue _ Cero = True
 mayorQue (Suc n) (Suc m) = mayorQue n m
+
+menorQue::Natural->Natural->Bool
+menorQue Cero Cero = False
+menorQue Cero _ = True
+menorQue _ Cero = False
+menorQue (Suc n) (Suc m) = menorQue n m
+
+igual::Natural->Natural->Bool
+igual Cero Cero = True
+igual Cero _ = False
+igual _ Cero = False
+igual (Suc n) (Suc m) = igual n m
