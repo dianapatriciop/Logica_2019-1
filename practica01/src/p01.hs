ebd20020d371b425f547d1c2859eca3d49861aa1
Daniel <daniel_lopez@ciencias.unam.mx>
2018-09-12 19:33:50 -0500

    agrego algunas funciones

--- practica01/src/p01.hs
+++ practica01/src/p01.hs
@@ -1,32 +1,37 @@
 data Natural = Cero | Suc Natural deriving (Eq, Show)
-
+--Dados dos naturales nos dice si el primero es mayor que el segundo.
 mayorQue::Natural->Natural->Bool
 mayorQue Cero Cero = False
 mayorQue Cero _ = False
 mayorQue _ Cero = True
 mayorQue (Suc n) (Suc m) = mayorQue n m
 
+--Dados dos naturales nos dice si el primero es menor que el segundo.
 menorQue::Natural->Natural->Bool
 menorQue Cero Cero = False
 menorQue Cero _ = True
 menorQue _ Cero = False
 menorQue (Suc n) (Suc m) = menorQue n m
 
+--Dados dos naturales nos dice si son iguales.
 igual::Natural->Natural->Bool
 igual Cero Cero = True
 igual Cero _ = False
 igual _ Cero = False
 igual (Suc n) (Suc m) = igual n m
 
+
+--Consideremos la siguiente definición de las listas de naturales.
 data ListaDeNaturales = Nil | Cons Natural ListaDeNaturales
---Dadas dos listas regresa la concatenación de ambas
+
+--Dadas dos listas de naturales regresar la concatenación de ambas.
 concate :: [Natural] -> [Natural] -> [Natural]
 concate [] [] = []
 concate [] x = x
 concate x [] = x
 concate l (x:xs) = concate (l++[x]) xs
 
---Dada una lista, regresa la reversa de la lista
+--Dada una lista regresar la reversa de dicha lista.
 reversa :: [Natural] -> [Natural]
 reversa [] = []
 reversa (x:xs) = reversa (xs) ++ [x]
@@ -41,8 +46,10 @@ data PL = Top |Bot | Var Indice
     |Oor PL PL
     |Oimp PL PL deriving (Eq, Show)
 
---conj :: PL -> [PL]
+--Dada una fórmula regresar una lista con las conjunciones de dicha fórmula
+--conj :: PL -> [PL]   
 
+--Dada una fórmula regresar el número de conjunciones que tiene dicha formula
 numConj :: PL -> Int
 numConj x = case x of  
     Top -> 0
@@ -53,3 +60,30 @@ numConj x = case x of
     Oor a b-> (numConj a) + (numConj b) 
     Oimp a b -> (numConj a) + (numConj b) 
 
+
+--Semántica
+--Consideremos los tipos de datos Valuación y Modelo de la forma
+type Valuacion = Indice -> Bool
+type Modelo = [Indice]
+
+--Dada una fórmula proposicional, evalúar la fórmula de acuerdo a un modelo
+--satMod :: Modelo -> PL -> Bool 
+
+--satPL :: Valuacion ->PL ->Bool 
+
+--Formas normales
+
+--Dada una fórmula nos indica si es una clausula.
+--esClausula :: PL ->Bool
+
+--Dada una fórmula nos indica si esta en forma normal de conjunción.
+--esCNF :: PL ->Bool
+
+--Dada una fórmula nos indica si es un término.
+--esTermino :: PL ->Bool
+
+--Dada una fórmula nos indica si esta en forma normal de disyunción.
+--esDNF :: PL ->Bool
+
+--Dada una fórmula en NNF y sin implicaciones, dar su CNF, tal que sean logicamente equivalentes
+--toCNF :: PL ->PL
