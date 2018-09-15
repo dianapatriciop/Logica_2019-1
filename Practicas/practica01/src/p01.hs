ee5dbfde5f59238b1e2be628866730f56ee7d40e
Daniel <daniel_lopez@ciencias.unam.mx>
2018-09-15 16:43:14 -0500

    agrego algunas otras funciones

--- Practicas/practica01/src/p01.hs
+++ Practicas/practica01/src/p01.hs
@@ -20,7 +20,6 @@ igual Cero _ = False
 igual _ Cero = False
 igual (Suc n) (Suc m) = igual n m
 
-
 --Consideremos la siguiente definición de las listas de naturales.
 data ListaDeNaturales = Nil | Cons Natural ListaDeNaturales
 
@@ -47,7 +46,7 @@ data PL = Top |Bot | Var Indice
     |Oimp PL PL deriving (Eq, Show)
 
 --Dada una fórmula regresar una lista con las conjunciones de dicha fórmula
---conj :: PL -> [PL]   
+--conj :: PL -> [PL]
 
 --Dada una fórmula regresar el número de conjunciones que tiene dicha formula
 numConj :: PL -> Int
@@ -67,14 +66,43 @@ type Valuacion = Indice -> Bool
 type Modelo = [Indice]
 
 --Dada una fórmula proposicional, evalúar la fórmula de acuerdo a un modelo
---satMod :: Modelo -> PL -> Bool 
+satMod :: Modelo-> PL -> Bool
+satMod m phi= case phi of 
+    Bot             -> False        
+    Top             -> True         
+    Var x           -> x `elem` m   
+    Oimp alpha beta -> not(satMod m alpha) || (satMod m beta)      
+    Oor alpha beta -> (satMod m alpha) || (satMod m beta)      
+    Oand alpha beta -> (satMod m alpha) && (satMod m beta)      
+    Oneg alpha -> not(satMod m alpha)    
 
 --satPL :: Valuacion ->PL ->Bool 
 
 --Formas normales
 
+--función auxiliar para saber si una fórmua es literal
+esLiteral :: PL -> Bool
+esLiteral x = case x of
+    Bot -> False 
+    Top -> False
+    Var y -> True  --es clausula si es Literal = <Variable> | Oneg <Variable>
+    Oimp alpha beta -> False
+    Oor alpha beta -> False
+    Oand alpha beta -> False
+    Oneg  alpha -> esLiteral alpha
+
+
 --Dada una fórmula nos indica si es una clausula.
---esClausula :: PL ->Bool
+esClausula :: PL ->Bool
+esClausula x = case x of
+    Bot -> True 
+    Top -> False
+    Var y -> True  --es clausula si es Literal = <Variable> | Oneg <Variable>
+    Oimp alpha beta -> False
+    Oor alpha beta -> (esLiteral alpha) || (esClausula beta) 
+    Oand aplha beta -> (False)
+    Oneg alpha -> esLiteral alpha
+
 
 --Dada una fórmula nos indica si esta en forma normal de conjunción.
 --esCNF :: PL ->Bool
